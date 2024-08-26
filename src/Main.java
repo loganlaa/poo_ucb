7cd518d2ef9926647238442e572a948f972f25ae
logan.araujo <logan.araujo@a.ucb.br>
2024-08-26 09:28:11 -0300

    teste

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        int n1 = 10;
+        double n2 = 10;
+
+
+        System.out.println("Hello and welcome!"); // imprime e pula linha
+        System.out.print("Hello and welcome!"); // imprime e nao pula linha
+        System.out.println();
+        System.out.printf("numero inteiro: %d  número decimal: %.1f", n1, n2); // imprime formatada igual C
+
+
+        }
+    }

31c5d09e16817bf76aba82cee7338b3d2a2a069b
logan.araujo <logan.araujo@a.ucb.br>
2024-08-26 10:01:34 -0300

    Fazendo soma com entrada de dados e JOptionPane

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -12,14 +13,11 @@ public class Main {
         String nome = "Logan";
         Scanner entrada = new Scanner(System.in); // definindo variável entrada da classe Scanner do java.util para entrada de dados
 
+        double num1 = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro número: ")); // JOptionPane é para ter a mensagem ser lançada em uma janela
+        double num2 = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo número: "));
 
-        System.out.println("Digite seu nome: ");
-        String nome2= entrada.nextLine(); // define a string nome2 que será definida pela variavel entrada. é específo para string por causa do nextLine()
-        System.out.print("Digite seu peso: ");
-        double peso = entrada.nextDouble();
-        System.out.println("Digite sua idade: ");
-        int idade = entrada.nextInt();
-        System.out.printf("Nome: %s, Peso: %.1f, idade: %d",nome2,peso, idade);
-
+        JOptionPane.showMessageDialog(parentComponent null, (num1 + num2));
         }
     }
+
+

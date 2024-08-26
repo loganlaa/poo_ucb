936d22e317b4a0755fea2eb530326fa3443e9d79
logan.araujo <logan.araujo@a.ucb.br>
2024-08-26 09:50:23 -0300

    Entrada de dados

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -7,13 +9,17 @@ public class Main {
 
         int n1 = 10;
         double n2 = 10;
+        String nome = "Logan";
+        Scanner entrada = new Scanner(System.in); // definindo variável entrada da classe Scanner do java.util para entrada de dados
 
 
-        System.out.println("Hello and welcome!"); // imprime e pula linha
-        System.out.print("Hello and welcome!"); // imprime e nao pula linha
-        System.out.println();
-        System.out.printf("numero inteiro: %d  número decimal: %.1f", n1, n2); // imprime formatada igual C
-
+        System.out.println("Digite seu nome: ");
+        String nome2= entrada.nextLine(); // define a string nome2 que será definida pela variavel entrada. é específo para string por causa do nextLine()
+        System.out.print("Digite seu peso: ");
+        double peso = entrada.nextDouble();
+        System.out.println("Digite sua idade: ");
+        int idade = entrada.nextInt();
+        System.out.printf("Nome: %s, Peso: %.1f, idade: %d",nome2,peso, idade);
 
         }
     }
